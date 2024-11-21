48fe255fabc1442f24f7bcad550b62cb46ea486e
Athirson <athirson.santos.sousa@gmail.com>
2024-11-20 22:04:06 -0300

    Update GreenGardenDB.sql

--- GreenGardenDB.sql
+++ GreenGardenDB.sql
@@ -34,10 +34,11 @@ estoque int
 create table cliente(
 cliente_id int not null primary key identity(1,1),
 cliente_nome varchar(60),
-email varchar(60),
+email varchar(60) not null,
 tel varchar(15),
 cpf varchar(11),
-endereco_id INT NOT NULL,
+endereco_id INT,
+nome_usuario varchar(30),
 senha VARCHAR(25) NOT NULL 
 FOREIGN KEY (endereco_id) REFERENCES Endereco(endereco_id)
 )
@@ -46,4 +47,5 @@ FOREIGN KEY (endereco_id) REFERENCES Endereco(endereco_id)
 SELECT * FROM Produtos
 select * from Endereco
 select * from Funcionario
+select * from cliente
 --<</exibir>>
